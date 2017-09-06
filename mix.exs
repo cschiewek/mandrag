444ced81cc05e85d83238a066df6811da2bcf3ad
Curtis Schiewek <curtis.schiewek@gmail.com>
2017-09-06 09:37:53 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mandrag.Mixfile do
   def project do
     [
       app: :mandrag,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "Deploy Elixir releases to Kubernetes",
