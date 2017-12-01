602044713acd6ad7e7f8dbe36954502725bf875f
Curtis <curtis.schiewek@gmail.com>
2017-12-01 17:00:11 -0500

    fix syntax

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mandrag.Mixfile do
   def project do
     [
       app: :mandrag,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "Build and Deploy Elixir releases to Kubernetes",
