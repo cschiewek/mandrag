d760487d2b2e636dcdb9a0bff994a0fac50c23a2
Curtis <curtis.schiewek@gmail.com>
2018-04-14 15:45:53 -0400

    rename

--- lib/mix/tasks/mandrag/apply.ex
+++ lib/mix/tasks/mandrag/apply.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Mandrag.UpdateKube do
   use Mix.Task
-  import Mandrag, only: [kubectl: 1, current: 0, name: 0, kube_config: 0]
-  @shortdoc "Sets the image"
+  import Mandrag, only: [helm: 1, current: 0, name: 0]
+  @shortdoc "Runs helm upgrade/install"
 
   def run(_) do
     helm "upgrade --install --set image.tag=#{current()} #{name()} ./chart "
