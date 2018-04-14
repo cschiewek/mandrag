2c0a06cf0b15ee01ad3d7e137e5bf8ffcb563c54
Curtis <curtis.schiewek@gmail.com>
2018-04-14 15:59:21 -0400

    working

--- lib/mandrag.ex
+++ lib/mandrag.ex
@@ -13,16 +13,9 @@ defmodule Mandrag do
 
   def docker(args), do: Mix.Shell.IO.cmd("docker " <> args)
 
-  def helm(args), do: Mix.Shell.IO.cmd("helm " <> args)
-
   def latest, do: "#{image()}:latest"
 
   def current, do: "#{image()}:#{Mix.Project.config[:version]}"
-
-  def pod do
-    {output, _} = System.cmd("kubectl", ~w"get pods")
-    output |> String.split("\n") |> Enum.find(&match/1) |> pod_name
-  end
 
   def migrate do
     if function_exported?(Ecto.Migrator, :run, 4) do
@@ -31,9 +24,4 @@ defmodule Mandrag do
       Ecto.Migrator.run(repo(), path, :up, all: true)
     end
   end
-
-  defp match(line), do: line =~ name()
-
-  defp pod_name(nil), do: nil
-  defp pod_name(pod_line), do: pod_line |> String.split(" ") |> Enum.at(0)
 end
--- lib/mix/tasks/mandrag/apply.ex
+++ lib/mix/tasks/mandrag/apply.ex
@@ -1,9 +1,9 @@
-defmodule Mix.Tasks.Mandrag.UpdateKube do
+defmodule Mix.Tasks.Mandrag.Apply do
   use Mix.Task
-  import Mandrag, only: [helm: 1, current: 0, name: 0]
+  import Mandrag, only: [current: 0, name: 0]
   @shortdoc "Runs helm upgrade/install"
 
   def run(_) do
-    helm "upgrade --install --set image.tag=#{current()} #{name()} ./chart "
+    Mix.Shell.IO.cmd "helm upgrade --install --set image.tag=#{current()} #{name()} ./chart "
   end
 end
