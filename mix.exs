02e8c9282bc5769517729700ec5c826a4ab77aba
Curtis <curtis.schiewek@gmail.com>
2018-04-15 12:04:22 -0400

    one more docker change

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mandrag.Mixfile do
   def project do
     [
       app: :mandrag,
-      version: "0.6.7",
+      version: "0.6.8",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Build and Deploy Elixir releases to Kubernetes via Helm",
