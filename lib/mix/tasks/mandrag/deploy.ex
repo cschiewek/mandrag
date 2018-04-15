6d0a3546b5c1b95541bb08fa985c85aaed70a112
Curtis <curtis.schiewek@gmail.com>
2018-04-15 11:08:58 -0400

    fix helm text

--- lib/mix/tasks/mandrag/deploy.ex
+++ lib/mix/tasks/mandrag/deploy.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Mandrag.Deploy do
   use Mix.Task
 
-  @shortdoc "Runs build, push, and apply, effectively building a releasing, pushing the image, and installing/updating your helm relase."
+  @shortdoc "Runs build, push, and upgrade, effectively building a release, pushing the image, and installing/updating your helm relase."
 
   def run(_) do
     Mix.Task.run("mandrag.docker.build")
