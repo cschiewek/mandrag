91cdb931c1d2f9c17d1fb4ffc9e2de2494c9f2fc
Curtis <curtis.schiewek@gmail.com>
2018-08-01 13:03:56 -0400

    add dockerfile export, and ci, bump to 0.6.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mandrag.Mixfile do
   def project do
     [
       app: :mandrag,
-      version: "0.6.10",
+      version: "0.6.11",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Build and Deploy Elixir releases to Kubernetes via Helm",
