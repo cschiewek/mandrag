8d800b012c07b32aeba7235b33c32f972cbcc884
Curtis <curtis.schiewek@gmail.com>
2018-09-10 18:48:09 -0400

    update to use distillery 2.x

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mandrag.Mixfile do
   def project do
     [
       app: :mandrag,
-      version: "0.6.11",
+      version: "0.7.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Build and Deploy Elixir releases to Kubernetes via Helm",
@@ -23,7 +23,7 @@ defmodule Mandrag.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:distillery, "~> 1.5.2", runtime: false},
+      {:distillery, "~> 2.0.3"},
       {:ecto, ">= 0.0.0", optional: true},
       {:ex_doc, "~> 0.16.3", only: :dev}
     ]
