425ab75565d4d5a9f003807a3217fa7e87f51f7a
Curtis <curtis.schiewek@gmail.com>
2018-09-15 15:38:55 -0400

    add context to helm

--- lib/mandrag/helm.ex
+++ lib/mandrag/helm.ex
@@ -2,6 +2,12 @@ defmodule Mandrag.Helm do
   @moduledoc """
   Documentation for Mandrag.Helm
   """
+  def context do
+    case Application.get_env(:mandrag, :k8s_context) do
+      nil -> ""
+      context -> "--kube-context #{context}"
+    end
+  end
 
-  def run(args), do: Mix.Shell.IO.cmd("helm " <> args)
+  def run(args), do: Mix.Shell.IO.cmd("helm #{context()} #{args}")
 end
--- test/mandrag/helm_test.exs
+++ test/mandrag/helm_test.exs
@@ -2,7 +2,8 @@ defmodule Mandrag.HelmTest do
   use ExUnit.Case, async: true
   alias Mandrag.Helm
 
-  # no helm in docker image
-  @tag :skip
+  test "context", do: assert(Helm.context() == "")
+
+  @tag :skip  # no helm in docker image
   test "run", do: assert(Helm.run("help &> /dev/null") == 0)
 end
