f657a82246b84ff09168810dbcc8c5087c697f00
Curtis <curtis.schiewek@gmail.com>
2018-09-15 16:20:37 -0400

    ugh, ecto apps already have a config key for ecto_repos, lets use that, and iterate over all those

--- lib/mandrag/ecto.ex
+++ lib/mandrag/ecto.ex
@@ -4,20 +4,13 @@ defmodule Mandrag.Ecto do
   """
   import Mandrag, only: [app: 0]
 
-  def repo, do: Application.get_env(:mandrag, :ecto_repo, default_repo())
-
-  defp default_repo do
-    module = app() |> Atom.to_string |> String.capitalize
-    String.to_existing_atom("#{module}.Repo")
-  rescue
-    ArgumentError -> nil
-  end
+  def repo, do: Application.get_env(app(), :ecto_repos)
 
   def migrate do
-    if function_exported?(Ecto.Migrator, :run, 4) do
+    if repo() && function_exported?(Ecto.Migrator, :run, 4) do
       {:ok, _} = Application.ensure_all_started(app())
       path = Application.app_dir(app(), "priv/repo/migrations")
-      Ecto.Migrator.run(repo(), path, :up, all: true)
+      Enum.map(repo(), &Ecto.Migrator.run(&1, path, :up, all: true))
     end
   end
 end
