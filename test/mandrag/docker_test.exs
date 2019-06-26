d5f48ca2c8dd2b56fb3fa1026d143203908b9740
Curtis <curtis.schiewek@gmail.com>
2019-06-25 22:09:08 -0400

    fix tests

--- test/mandrag/docker_test.exs
+++ test/mandrag/docker_test.exs
@@ -24,7 +24,7 @@ defmodule Mandrag.DockerTest do
   test "latest", do: assert(Docker.latest() == "mandrag:latest")
 
   test "export" do
-    assert Docker.export() == 661
+    assert Docker.export() == 757
     assert File.exists?("Dockerfile")
     File.rm!("Dockerfile")
   end
--- test/mandrag/ecto_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Mandrag.EctoTest do
-  use ExUnit.Case, async: true
-  alias Mandrag.Ecto
-
-  test "repos", do: assert(Ecto.repos() == nil)
-
-  @tag :skip
-  test "migrate", do: assert(Ecto.migrate() == nil)
-end
